ab4b0c10389e51d89adf91b2d505dc914abe39cb
Abhinav Maharana <abhinavmaharana.004@gmail.com>
2023-02-11 01:17:19 +0530

    🎉 Basic setup

---
+++ truffle-config.js
@@ -0,0 +1,47 @@
+// require('dotenv').config();
+// const { MNEMONIC, PROJECT_ID } = process.env;
+
+// const HDWalletProvider = require('@truffle/hdwallet-provider');
+
+module.exports = {
+  // React only allows importing the built smart contract code (abis) from the src directory
+  contracts_build_directory: path.join(__dirname, 'src/abis'),
+  networks: {
+    development: {
+      host: '127.0.0.1', //localhost
+      port: 7545, // default port for ganache dev server
+      network_id: '*', // matches any network id
+    },
+  },
+
+  // Set default mocha options here, use special reporters, etc.
+  mocha: {
+    // timeout: 100000
+  },
+
+  // Configure your compilers
+  compilers: {
+    solc: {
+      version: "0.8.17" // Fetch exact version from solc-bin (default: truffle's version)
+      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
+      // settings: {          // See the solidity docs for advice about optimization and evmVersion
+      //  optimizer: {
+      //    enabled: false,
+      //    runs: 200
+      //  },
+      //  evmVersion: "byzantium"
+      // }
+    }
+  }
+
+  // db: {
+  //   enabled: false,
+  //   host: "127.0.0.1",
+  //   adapter: {
+  //     name: "indexeddb",
+  //     settings: {
+  //       directory: ".db"
+  //     }
+  //   }
+  // }
+};
